bdc4edd3a1448db5ade12b52070ec2c3a4015f2e
Administrador <Administrador@DESKTOP-939J3ID>
2021-06-28 22:11:24 -0500

    Update 4.0

--- src/test/java/com/indra/maven/example/AppTest.java
+++ src/test/java/com/indra/maven/example/AppTest.java
@@ -4,9 +4,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.junit.Before;
 import org.junit.Test;
-
-import com.indra.maven.example.App;
-
 import org.junit.After;
 import static org.junit.Assert.*;
 
