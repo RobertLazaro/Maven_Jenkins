de6f87a12a979a0703579f45b4c11d74794afbed
RobertoLazaro <robertolazaro974@gmail.com>
2021-06-28 19:11:33 -0500

    Update

---
+++ src/main/java/com/indra/maven/example/App.java
@@ -0,0 +1,10 @@
+package com.indra.maven.example;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("prueba trabajo maven y jenkins.");
+	}
+
+}
